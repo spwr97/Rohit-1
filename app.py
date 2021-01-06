a8da3b66042855fc12c2843be7a04a10b5f1092d
prabodhw96 <prabodhwankhede1996@gmail.com>
2021-01-07 00:13:03 +0530

    Find by name

--- app.py
+++ app.py
@@ -18,6 +18,10 @@ def load_data():
 
 data = load_data()
 
+# Show data of entire region
+st.map(data)
+
+# Show data for selected villages and family size
 village_names = list(data.Village.value_counts().index)
 village_names.sort()
 village_names.insert(0, "All villages")
@@ -33,10 +37,8 @@ if st.checkbox("Select Family size", False):
 
 midpoint = (np.average(data["latitude"]), np.average(data["longitude"]))
 
-st.map(data)
-
 st.write(pdk.Deck(
-	map_style="mapbox://styles/mapbox/light-v9",
+    map_style="mapbox://styles/mapbox/light-v9",
     initial_view_state={
         "latitude": midpoint[0],
         "longitude": midpoint[1],
@@ -56,6 +58,23 @@ st.write(pdk.Deck(
         ),
     ],
 ))
+
+# Show name and location of the person
+st.header("Locate person by name in: "+village)
+name_list = list(set(data["Patron"].value_counts().index))
+name_list.sort()
+name = st.selectbox("Names", name_list)
+
+data = data[data["Patron"]==name]
+
+fig = px.scatter_mapbox(data,
+                        lat="latitude", lon="longitude",
+                        hover_name="Patron", hover_data=["Village"],
+                        color_discrete_sequence=["fuchsia"], zoom=10, height=500)
+fig.update_traces(marker_size=12)
+fig.update_layout(mapbox_style="open-street-map")
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+st.write(fig)
 
 if st.checkbox("Show Raw Data", False):
     st.subheader("Raw Data")
